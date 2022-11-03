0e6cc9a53f1eb4ff7645d58fc9ed5fdac41e8f1a
rsharm18 <40524914+rsharm18@users.noreply.github.com>
2022-11-02 23:26:14 -0500

    changes related to publishing the ride request to nearby taxi

--- taxi_management_service/service/com/taxicoop/service/taxi_service.py
+++ taxi_management_service/service/com/taxicoop/service/taxi_service.py
@@ -1,4 +1,5 @@
 import traceback
+from os import getenv
 
 from com.taxicoop.dto.RegisterNewLocationDTO import RegisterNewLocationDTO
 from com.taxicoop.dto.RegisterNewTaxiDTO import RegisterNewTaxiDTO
@@ -7,6 +8,16 @@ from com.taxicoop.model.Taxi import Taxi
 from com.taxicoop.model.Taxi import Taxi_Type, GeoData
 from com.taxicoop.service.DBHelper import DB_Helper
 from com.taxicoop.model.Taxi import Taxi_Status
+from taxicoop.dto.RequestBookTaxiDTO import RequestBookTaxiDTO
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+# TAXI_BASE_URL = "http://taxicoop-api-load-balancer-898563336.us-east-1.elb.amazonaws.com/api/taxis/v1" #  getenv('TAXI_SERVICE_BASE_URL')
+RIDE_REQUEST_SERVICE_BASE_URL = getenv('RIDE_REQUEST_SERVICE_BASE_URL', 'http://localhost:8080/api/rides/v1')
+
+print(" RIDE_REQUEST_SERVICE_BASE_URL {}".format(RIDE_REQUEST_SERVICE_BASE_URL))
 
 
 class Taxi_Service:
@@ -33,7 +44,7 @@ class Taxi_Service:
         user_location = GeoData(user_longitude, user_latitude)
         return DB_Helper.get_near_by_taxis(user_location.__dict__, vehicle_type.value)
 
-    def reserve(self, taxi_id):
+    def reserve(self, taxi_id, ride_req: RequestBookTaxiDTO):
         error = {'status': 'failed', 'message': 'Error confirming the ride'}
         print(" reserve taxi - START")
         try:
@@ -46,8 +57,21 @@ class Taxi_Service:
                 if not status == Taxi_Status.AVAILABLE.value:
                     return {'status': 'failed', 'message': 'Taxi is not available. The taxi is {}'.format(status)}
 
-                DB_Helper.update_taxi(taxi_id, {'status': Taxi_Status.RIDE_IN_PROGRESS.value})
-                return {'status': 'success', 'message': 'Ride in Progress'}
+                url = '{}/{}/confirm_ride'.format(RIDE_REQUEST_SERVICE_BASE_URL, ride_req.ride_request_id)
+                payload = {
+                    'taxi_id': taxi_id,
+                    'ride_request_id': ride_req.ride_request_id,
+                    'vehicle_type': taxi['type']
+                }
+
+                print(" Calling Ride_Mgmt service  {} ".format(url))
+                result = requests.post(url, json=payload).json()
+                print("confirm_ride request response {}".format(result))
+                if 'success' == result['status']:
+                    DB_Helper.update_taxi(taxi_id, {'status': Taxi_Status.RIDE_IN_PROGRESS.value})
+                    return {'status': 'success', 'message': 'Ride in Progress'}
+
+                return {'status': 'failed', 'message': "ERROR :: {}".format(result['message'])}
             else:
                 return error
         except Exception as ex:
